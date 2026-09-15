1df4e8761f540c9e6634e3f169cb31f6aa9919cc
Sujit Kumar Libi <sujit_libi@hotmail.com>
2026-09-15 18:55:34 +0545

    Project 14: NASA APOD API integration added

---
+++ nasa-apod/script.js
@@ -0,0 +1,140 @@
+const resultsNav = document.getElementById("resultsNav");
+const favoritesNav = document.getElementById("favoritesNav");
+const imagesContainer = document.querySelector(".images-container");
+const saveConfirmed = document.querySelector(".save-confirmed");
+const loader = document.querySelector(".loader");
+
+// NASA API
+count = 2;
+const apiKey = "DEMO_KEY";
+const apiUrl = `https://science.nasa.gov/wp-json/wp/v2/apod-basic/?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
+let favorites = {};
+
+function showContent(page) {
+  window.scrollTo({
+    top: 0,
+    behavior: "instant",
+  });
+  if (page === "results") {
+    resultsNav.classList.remove("hidden");
+    favoritesNav.classList.add("hidden");
+  } else {
+    resultsNav.classList.add("hidden");
+    favoritesNav.classList.remove("hidden");
+  }
+  loader.classList.add("hidden");
+}
+
+function createDOMNodes(page) {
+  currentArray = page === "results" ? resultsArray : Object.values(favorites);
+  currentArray.forEach((result) => {
+    // Card container
+    const card = document.createElement("div");
+    card.classList.add("card");
+    // Link
+    const link = document.createElement("a");
+    link.href = result.url;
+    link.title = "View Full Image;";
+    link.target = "_blank";
+    // Image
+    const image = document.createElement("img");
+    image.src = result.hdurl;
+    image.alt = "NASA Picture of the Day";
+    image.loading = "lazy";
+    image.classList.add("card-img-top");
+    // Card Body
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    // Card title
+    const cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-title");
+    cardTitle.textContent = result.title;
+    // Save Text
+    const saveText = document.createElement("p");
+    saveText.classList.add("clickable");
+    if (page === "results") {
+      saveText.textContent = "Add to Favorites";
+      saveText.setAttribute("onclick", `saveFavorite('${result.url}')`);
+    } else {
+      saveText.textContent = "Remove Favorites";
+      saveText.setAttribute("onclick", `removeFavorite('${result.url}')`);
+    }
+
+    // Card Text
+    const cardText = document.createElement("p");
+    cardText.innerHTML = result.explanation;
+    // Footer Container
+    const footer = document.createElement("small");
+    footer.classList.add("text-muted");
+    // Date
+    const date = document.createElement("strong");
+    date.textContent = result.date;
+    // Copyright
+    const copyrightResult =
+      result.copyright === undefined ? "" : result.copyright;
+    const copyright = document.createElement("span");
+    copyright.innerHTML = ` ${copyrightResult}`;
+    // Append
+    footer.append(date, copyright);
+    cardBody.append(cardTitle, cardText, saveText, footer);
+    link.appendChild(image);
+    card.append(link, cardBody);
+    imagesContainer.appendChild(card);
+  });
+}
+
+// Lets Update DOM with result
+function updateDOM(page) {
+  // Get Favorites from localStorage
+  if (localStorage.getItem("nasaFavorites")) {
+    favorites = JSON.parse(localStorage.getItem("nasaFavorites"));
+  }
+  imagesContainer.textContent = "";
+  createDOMNodes(page);
+  showContent(page);
+}
+
+// Add result to Favourite
+function saveFavorite(itemUrl) {
+  // Loop through Results Array to select Favorite
+  resultsArray.forEach((item) => {
+    if (item.url.includes(itemUrl) && !favorites[itemUrl]) {
+      favorites[itemUrl] = item;
+      // Show Save Confirmation for 2 seconds
+      saveConfirmed.hidden = false;
+      setTimeout(() => {
+        saveConfirmed.hidden = true;
+      }, 2000);
+      // Set Favorites in localStorage
+      localStorage.setItem("nasaFavorites", JSON.stringify(favorites));
+    }
+  });
+}
+
+// Get 10 Images from NASA API
+async function getNasaPictures() {
+  // show loader
+  loader.classList.remove("hidden");
+  try {
+    const response = await fetch(apiUrl);
+    resultsArray = await response.json();
+    updateDOM("results");
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// Remove item from Favorites
+function removeFavorite(itemUrl) {
+  if (favorites[itemUrl]) {
+    delete favorites[itemUrl];
+    // Set Favorites in localStorage
+    localStorage.setItem("nasaFavorites", JSON.stringify(favorites));
+    updateDOM("favorites");
+  }
+}
+
+// On Load
+getNasaPictures();
