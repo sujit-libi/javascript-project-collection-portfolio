8c02926e8bd7d7e85ac3b4c49e4bfadab7910cfe
Sujit Kumar Libi <sujit_libi@hotmail.com>
2026-09-16 12:55:45 +0545

    Project 16: Math Sprint Game added

---
+++ math-sprint-game/shuffle.js
@@ -0,0 +1,18 @@
+function shuffle(array) {
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
